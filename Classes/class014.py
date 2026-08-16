468422a768e046302c9615f05a90957ddde2d8bf
Joaquim <joaquimlima0612@gmail.com>
2026-08-15 23:57:29 -0300

    Class on Debugging

--- Classes/class014.py
+++ Classes/class014.py
@@ -1,1 +1,12 @@
 # ============== DEBUGGING ==============
+
+def main():
+    height = int(input('Height: '))
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+        print('#' * (i + 1)) # build the pyramid with a defined height
+
+if (__name__ == '__main__'): # Python has a special variable called "__name__", when this file's executed directly, gets the value "__main__" automatically. So, if this program is executed at here, execute main()
+    main()
